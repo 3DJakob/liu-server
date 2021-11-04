079eb06265cdc10fd5317d1789578f8264b7f9c3
Jakob Unnebäck <jakob@folkdatorn.se>
2021-11-04 21:11:45 +0100

    Standard style

--- index.js
+++ index.js
@@ -5,30 +5,29 @@ const axios = require('axios')
 require('dotenv').config()
 
 app.get('/', async (req, res) => {
-    const stats = await getStatistics()
-    res.json(stats)
+  const stats = await getStatistics()
+  res.json(stats)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
 
-
 const getStatistics = async () => {
-    const options = {
-        method: 'GET',
-        url: process.env.API_URI + '/ExamStatistics',
-        params: { courseCode: 'TNM040', moduleCode: 'PRA1' },
-        headers: {
-            'LiU-Operation-Id': process.env.LIU_OPERATION_ID,
-            'LiU-Api-Version': 'v1',
-            'Ocp-Apim-Subscription-Key': process.env.SUBSCRITPION_KEY
-        },
-        data: '{body}'
-    };
+  const options = {
+    method: 'GET',
+    url: process.env.API_URI + '/ExamStatistics',
+    params: { courseCode: 'TNM040', moduleCode: 'PRA1' },
+    headers: {
+      'LiU-Operation-Id': process.env.LIU_OPERATION_ID,
+      'LiU-Api-Version': 'v1',
+      'Ocp-Apim-Subscription-Key': process.env.SUBSCRITPION_KEY
+    },
+    data: '{body}'
+  }
 
-    const response = await axios.request(options).catch(function (error) {
-        console.error(error);
-    });
-    return await response.data
+  const response = await axios.request(options).catch(function (error) {
+    console.error(error)
+  })
+  return await response.data
 }
